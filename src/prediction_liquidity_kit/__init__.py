6d6348bbf413d21acaff63263a2068118f842622
Sang Truong <45520864+sangtrx@users.noreply.github.com>
2026-09-24 13:17:15 +0700

    #744 bump package version to 0.1.0

--- src/prediction_liquidity_kit/__init__.py
+++ src/prediction_liquidity_kit/__init__.py
@@ -1,3 +1,1 @@
-"""Prediction-market liquidity incentive and simulation toolkit."""
-
-__version__ = "0.0.1"
+"""Prediction-market liquidity incentive and simulation toolkit."""\n\n__version__ = "0.1.0"\n
